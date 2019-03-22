8766477b02ebbe8b6e4ca9ad31a4a4276b5c1938
siddharths2k <ss2k@mit.edu>
2019-03-22 19:34:20 -0400

    Added getWords.js

--- getWords.js
+++ getWords.js
@@ -1,11 +1,24 @@
 
-var getWords = function(titles){
-
+var getWords = function(titles)
+{
 	// -----------------------------------
-	//  YOUR CODE
-	//  Filter out punctuation/numbers
-	//  Return words array
+	//  This function removes punctuation 
+	//  and numbers from each course title
+	//  and returns an array of arrays of
+	//  words of each title
 	// -----------------------------------	
+
+	var words = titles.map(function(title)
+	{
+		// Convert to lower case
+		var edited_title = title.toLowerCase();
+		// Select only lower case characters and spaces
+		edited_title = title.match(/[a-z]\s/g);
+		// Remove extra spaces at start and end
+		edited_title = edited_title.trim();
+		// Return array of words
+		return edited_title.split(' ');
+	});
 
 	return words;
 }
