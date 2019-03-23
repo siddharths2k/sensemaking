75aaee998721166e49aebad0b4efac9949286326
siddharths2k <ss2k@mit.edu>
2019-03-22 23:25:02 -0400

    Added getScores.js

--- getScores.js
+++ getScores.js
@@ -2,14 +2,30 @@
 var getScores = function(wordsFlat){
 
 	// -----------------------------------
-	//  YOUR CODE
-	//  Count the word frequency
-	//  Return object with word counts
+	//  This function returns an object
+	//  containing words and their 
+	//  corresponding frequencies
 	// -----------------------------------	
 
-	return scores;
+	scores = wordsFlat.reduce(function(frequency_list,word)
+	{
+		// If the word has been added to the object already
+		if (word in frequency_list)
+		{
+			// Increase frequency by 1
+			frequency_list[word] += 1;
+		}		
+		// If the word has not been added to the object yet
+		else
+		{
+			// Set frequency as 1
+			frequency_list[word] = 1;
+		}
+		// Return updated list of word frequencies
+		return frequency_list;
+	},{});
 
+	return scores; 
 }
-
 
 module.exports = getScores;
