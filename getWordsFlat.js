c69fef0964b670549e6f818f9270a39f56ffa7e9
siddharths2k <ss2k@mit.edu>
2019-03-22 22:29:17 -0400

    Added getWordsFlat.js

--- getWordsFlat.js
+++ getWordsFlat.js
@@ -2,10 +2,27 @@
 var getWordsFlat = function(words){
 
 	// -----------------------------------
-	//  YOUR CODE
-	//  Flatten the 2D words array using reduce
-	//  Return flat words array
+	//  This function obtains a list of
+	// 	words in all course titles
 	// -----------------------------------	
+
+	wordsFlat = words.reduce(function(word_list,words_in_course)
+	{
+		// Iterate through each word in the array of 
+		// words in one course name
+		words_in_course.forEach(function(word_in_course)
+		{
+			// If the word is not present in the reduced list
+			if(!word_list.includes(word_in_course))
+			{
+				// Append the word to the reduced list
+				word_list.push(word_in_course);
+			}
+		});
+		// Return new reduced list of words after adding 
+		// new words of current course name
+		return word_list;
+	},[]);
 
 	return wordsFlat;
 }
